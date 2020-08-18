118b3e4be6c379559396a741220aeba87721df6a
Jiangqi Li <jiangqi319@gmail.com>
2020-08-18 13:43:38 -0400

    add regu condition

--- app/@core/utils/stringHelper.js
+++ app/@core/utils/stringHelper.js
@@ -1,6 +1,8 @@
 module.exports = {
 
 	isPalindrome: function(str){
+		//str = str.replace(/[^\w]/gi, '');
+		console.log(str);
 		let length = str.length;
 		for (let i = 0; i < length/2; i++) {
 			if (str[i].toLowerCase() !== str[length - 1 - i].toLowerCase()) {
